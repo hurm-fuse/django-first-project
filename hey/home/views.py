cb56bd2d15969addc04594af07ae798dfd4c469a
Hurm Fuse <hurmfuse@gmail.com>
2026-02-01 19:23:29 +0000

    h

---
+++ hey/home/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+def index(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message_text = request.POST.get('message')
+
+        contact = Contact(name=name, email=email, message=message_text, date=datetime.today())
+        contact.save()
+        messages.success(request, "Your message has been sent!")
+        return redirect('/')
+    context = {'price': "$10,000"}
+    return render(request, 'index.html', context)
+
+def about(request):
+    return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def buy(request):
+    return render(request, 'buy.html')
